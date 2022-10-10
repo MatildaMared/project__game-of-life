df088bdcedd4ca7c8c0ccb0d99b54cee04965b5a
Matilda Mared <matildamared@live.se>
2022-10-10 08:19:11 +0200

    change grid cells to be buttons instead for accessibility reasons

--- src/components/GameBoard/GameBoard.tsx
+++ src/components/GameBoard/GameBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import useGameBoard from "../../hooks/useGameBoard";
 
@@ -7,8 +7,11 @@ function GameBoard() {
 	const { grid, toggleCell } = useGameBoard(numberOfRows);
 
 	return (
-		<div>
-			GameBoard
+		<Container>
+			<WelcomeText>Welcome to the game of life.</WelcomeText>
+			<Introduction>
+				Click the explanation button to learn the rules and have fun playing!
+			</Introduction>
 			<Grid numberOfRows={numberOfRows}>
 				{grid &&
 					grid.map((rows, i) =>
@@ -21,9 +24,13 @@ function GameBoard() {
 						))
 					)}
 			</Grid>
-		</div>
+		</Container>
 	);
 }
+
+const Container = styled.div`
+	margin-top: 32px;
+`;
 
 interface GridProps {
 	numberOfRows: number;
@@ -37,12 +44,13 @@ const Grid = styled.div<GridProps>`
 	grid-template-columns: repeat(${(props) => props.numberOfRows * 2}, 1fr);
 `;
 
-const Cell = styled.div`
+const Cell = styled.button`
 	width: 25px;
 	height: 25px;
 	background-color: var(--color-gray-900);
 	transition: background-color 250ms;
 	cursor: pointer;
+	border: none;
 
 	&:hover {
 		background-color: var(--color-gray-800);
@@ -51,6 +59,27 @@ const Cell = styled.div`
 	&.alive {
 		background-color: var(--color-pink);
 	}
+
+	&:focus {
+		outline: 2px solid var(--color-pink-transparent);
+		outline-offset: 2px;
+		z-index: 1;
+	}
+`;
+
+const WelcomeText = styled.h3`
+	font-family: var(--font-secondary);
+	margin: 0 auto;
+	text-align: center;
+	margin-bottom: 8px;
+`;
+
+const Introduction = styled.p`
+	text-align: center;
+	width: 30ch;
+	margin: 0 auto;
+	margin-bottom: 16px;
+	color: var(--color-gray-300);
 `;
 
 export default GameBoard;
--- src/styles/Variables.ts
+++ src/styles/Variables.ts
@@ -7,11 +7,19 @@ const Variables = createGlobalStyle`
 
         --color-background: hsla(277, 6%, 12%, 1.0);
         --color-text: hsla(277, 6%, 85%, 1.0);
+
         --color-gray-900: hsla(280, 4%, 13%, 1.0);
         --color-gray-800: hsla(277, 7%, 16%, 1.0);
         --color-gray-700: hsla(277, 7%, 18%, 1.0);
+        --color-gray-600: hsla(277, 7%, 23%, 1.0);
+        --color-gray-500: hsla(277, 7%, 30%, 1.0);
+        --color-gray-400: hsla(277, 7%, 40%, 1.0);
+        --color-gray-300: hsla(277, 7%, 50%, 1.0);
+        --color-gray-200: hsla(277, 7%, 60%, 1.0);
+        --color-gray-100: hsla(277, 7%, 70%, 1.0);
 
         --color-pink: hsla(327, 100%, 77%, .8);
+        --color-pink-transparent: hsla(327, 100%, 77%, .6);
     }
 `;
 
