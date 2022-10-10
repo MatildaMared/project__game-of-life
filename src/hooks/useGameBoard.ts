8089862d31f4f71d616066cdbef285385d77c626
Matilda Mared <matildamared@live.se>
2022-10-10 11:10:41 +0200

    add rules when initiating game board

--- src/hooks/useGameBoard.ts
+++ src/hooks/useGameBoard.ts
@@ -1,12 +1,24 @@
 import { useState, useEffect } from "react";
 import { Position } from "../interfaces/Position";
 import { GameOfLife } from "../logic/GameOfLife/GameOfLife";
+import DeadCellWithThreeNeighbors from "../logic/rules/DeadCellWithThreeNeighbors";
+import LivingCellWithFourOrMoreNeighbors from "../logic/rules/LivingCellWithFourOrMoreNeighbors";
+import LivingCellWithOneOrLessNeighbors from "../logic/rules/LivingCellWithOneOrLessNeighbors";
+import LivingCellWithTwoOrThreeNeighbors from "../logic/rules/LivingCellWithTwoOrThreeNeighbors";
 
 type Grid = number[][];
 
 function useGameBoard(numberOfRows: number) {
 	const [grid, setGrid] = useState<Grid>([]);
-	const [gameOfLife] = useState(() => new GameOfLife());
+	const [gameOfLife] = useState(
+		() =>
+			new GameOfLife([
+				new DeadCellWithThreeNeighbors(),
+				new LivingCellWithFourOrMoreNeighbors(),
+				new LivingCellWithTwoOrThreeNeighbors(),
+				new LivingCellWithOneOrLessNeighbors(),
+			])
+	);
 
 	function resetGame() {
 		setGrid(gameOfLife.createInitialGrid(numberOfRows));
