b47aba5d4282d01eabf0dc947abac3ffdd0d7d98
Matilda Mared <matildamared@live.se>
2022-10-10 10:39:18 +0200

    add calculate number of neighbors method

--- src/logic/GameOfLife/GameOfLife.ts
+++ src/logic/GameOfLife/GameOfLife.ts
@@ -1,4 +1,5 @@
 import { Position } from "../../interfaces/Position";
+import { Rule } from "../../interfaces/Rule";
 import { Grid } from "../../types/Grid";
 
 export class GameOfLife {
@@ -18,5 +19,35 @@ export class GameOfLife {
 		newGrid[row][column] = newGrid[row][column] === 0 ? 1 : 0;
 
 		return newGrid;
+	}
+
+	applyRules(
+		numberOfNeighbors: number,
+		isAlive: boolean,
+		rules: Rule[]
+	): boolean {
+		return false;
+	}
+
+	calculateNextFrame(currentGrid: Grid): Grid {
+		const newGrid = [...currentGrid];
+
+		return newGrid;
+	}
+
+	calculateNumberOfNeighbors(grid: Grid, position: Position): number {
+		const { row, column } = position;
+		let numberOfNeighbors = 0;
+
+		grid[row - 1][column - 1] === 1 && numberOfNeighbors++;
+		grid[row - 1][column] === 1 && numberOfNeighbors++;
+		grid[row - 1][column + 1] === 1 && numberOfNeighbors++;
+		grid[row][column - 1] === 1 && numberOfNeighbors++;
+		grid[row][column + 1] === 1 && numberOfNeighbors++;
+		grid[row + 1][column - 1] === 1 && numberOfNeighbors++;
+		grid[row + 1][column] === 1 && numberOfNeighbors++;
+		grid[row + 1][column + 1] === 1 && numberOfNeighbors++;
+
+		return numberOfNeighbors;
 	}
 }
